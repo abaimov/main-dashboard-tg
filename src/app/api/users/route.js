bbaa6a4750b4005e1915a79cec3da03f4638da3c
rickmoralis <rickmoralis@rickmoraliss-iMac.local>
2024-08-20 21:21:10 +0300

    return dynamic routes

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from "../../../../prisma/client";
-// export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
 
 
 export async function GET() {
